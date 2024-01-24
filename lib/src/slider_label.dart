82385814a49c05c57ef8db29208a4b9f6304ed2b
John Damits <kakoijohn@gmail.com>
2024-01-24 15:09:35 +0900

    Update slider_label.dart

--- lib/src/slider_label.dart
+++ lib/src/slider_label.dart
@@ -27,6 +27,7 @@ class SliderLabel extends StatelessWidget {
     final roundedVal = value.round().toInt();
     widgets.add(
       Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Text('$roundedVal', style: appearance.infoMainLabelStyle),
           Text('$modifier', style: appearance.modifierLabelStyle),
